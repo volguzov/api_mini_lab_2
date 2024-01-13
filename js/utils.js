9d78a35a04af983eb318fc5b9543b0db5d938cfa
volguzov <117124526+volguzov@users.noreply.github.com>
2024-01-13 21:34:55 +0300

    Add files via upload

--- js/utils.js
+++ js/utils.js
@@ -1,51 +1,39 @@
-const formValues = {}  // Сюда пишутся значения формы (Object как в Java, или dict из Python)
-const formValidation = {}  // Сюда пишутся статусы валидации каждого поля. Если поле ни разу не валидировалось,
-// то при обращении к Object вернётся undefined, который при логическом сравнении обрабатывается как false
+const formValues = {}
+const formValidation = {}
 
-
-// Объявляется и инициализируется константная переменная
-// Инициализация функцией, заданной в стрелочном виде
-export const validatePassword = (e) => {
-  formValidation.password = e.target.value
-  console.log("Password validation...")
-  console.log(e)
-  // Напишите код валидации здесь и присвойте true/false в объект(словарь) formValidation
-  // formValidation.password = ...  // formValidation['password'] = ... - то же самое, но другой синтаксис
-  return formValidation.password !== undefined   // Это заглушка, return вероятно надо переписать
+export const validatePassword = (value) => {
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  return regex.test(value);
 }
 
-
 export const validateEmail = (email) => {
-  // Создадим шаблон регулярного выражения. В нём применяются шаблонные строки
-  // Гуглить по тегам: "шаблонные строки js", "регулярные выражения"
   const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
   return String(email)
-    .toLowerCase()
-    .match(regExp);
+      .toLowerCase()
+      .match(regExp);
 }
 
-
-// Функция возвращающая true если все валидации пройдены, и false если хотя бы одна не пройдена
 export const getValidationStatus = () => {
-  // Происходит функциональная мгаия, читай строчку кода ниже как:
-  // Получить значения (не ключи) из объекта, затем применить к каждому значению функцию двойного логического отрицания
-  // (преобразование к булевому типу) и результаты всех применений это true, то вернуть true, иначе - false
   return Object.values(formValidation).every((validationStatus) => !!validationStatus)
 }
 
-
-// Функция возвращающая которая ставит значение поля в форме по ключу
-export const setFormValue = (valueKey, newValue, validator) => {
-  formValues[valueKey] = newValue
-  if (validator !== undefined) {
-    formValidation[valueKey] = validator(newValue)
+export const setFormValue = (id, value, validator) => {
+  formValues[id] = value;
+  if (validator) {
+    const isValid = validator(value);
+    formValidation[id] = isValid;
+    const element = document.getElementById(id);
+    element.classList.remove('invalid');
+    element.classList.remove('valid');
+    if (isValid) {
+      element.classList.add('valid');
+    } else {
+      element.classList.add('invalid');
+    }
   }
 }
 
-
-// Функция для обработки отправки формы регистрации
-// В этой функции должен быть http запрос на сервер для регистрации пользователя (сейчас просто демонстрация)
 export const submitSignUpForm = () => {
   if (!getValidationStatus()) {
     console.log("FORM IS INCORRECT")
